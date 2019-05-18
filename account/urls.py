ee2032d5db463c29ef2928d02d5479e6733d19ab
Florent <grenaille.florent85@gmail.com>
2019-05-18 18:56:30 +0200

    Ajout de la page d'inscription (front-end fini)

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'account'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup', views.sign_up, name='sign_up'),
 ]
--- account/views.py
+++ account/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def  index(request):
     return render(request, 'account/index.html')
+
+def sign_up(request):
+    return render(request, 'account/sign_up.html')
