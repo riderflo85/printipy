b799d83f640ecf5e9591525e7550a0148a8d9eeb
Florent <grenaille.florent85@gmail.com>
2019-05-28 19:48:00 +0200

    Ajout de la gestion de l'inscription utilisateur et de la déconnexion utilisateur. Ajout de 'sign_out.html'

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,6 @@ app_name = 'account'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.sign_up, name='sign_up'),
+    path('signout', views.sign_out, name='sign_out'),
     path('minidashboard', views.mini_dashboard, name='minidashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
+from django.contrib.auth.models import User
+from .forms import LoginForm, SignupForm
 
 
 def  index(request):
@@ -27,7 +28,36 @@ def  index(request):
     return render(request, 'account/index.html', context)
 
 def sign_up(request):
-    return render(request, 'account/sign_up.html')
+    context = {'errors': False}
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            nom = form.cleaned_data['last_name']
+            prenom = form.cleaned_data['first_name']
+            pseudo = form.cleaned_data['pseudo']
+            email = form.cleaned_data['email']
+            pwd = form.cleaned_data['password']
+
+            new_user = User.objects.create_user(pseudo, email, pwd)
+            new_user.last_name = nom
+            new_user.first_name = prenom
+            new_user.save()
+            context['new_user'] = new_user
+
+        else:
+            context['errors'] = form.errors.items()
+
+    else:
+        form = SignupForm()
+    context['form'] = form
+
+    return render(request, 'account/sign_up.html', context)
+
+def sign_out(request):
+    logout(request)
+    return render(request, 'account/sign_out.html')
 
 def mini_dashboard(request):
     return render(request, 'account/mini_dashboard.html')
