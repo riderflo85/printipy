b342c34ca7d733f58cd08e94e81ad6f785dbe062
Florent <grenaille.florent85@gmail.com>
2019-06-06 17:09:53 +0200

    Modification des settings pour l'ajout de la configuration de la BDD en local

--- printipy_project/settings.py
+++ printipy_project/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'printipy_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'printipy_db',
+#     'USER': 'pi',
+#     'PASSWORD': 'tiflo300',
+#     'HOST': '192.168.1.17',
+#     'PORT': '5432',
+#     }
+# }
+
+# Configuration de la base de données en local
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'printipy_db',
-        'USER': 'pi',
+    'USER': 'pi',
     'PASSWORD': 'tiflo300',
-    'HOST': '192.168.1.17',
+    'HOST': '',
     'PORT': '5432',
     }
 }
