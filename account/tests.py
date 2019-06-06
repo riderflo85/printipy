57179c7892139ccb7d42e23df2dcc5a80a4ef718
Florent <grenaille.florent85@gmail.com>
2019-06-06 17:55:54 +0200

    Création de test pour les formulaires et pour les codes de statu des pages web

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .forms import LoginForm, SignupForm
 
 # Create your tests here.
+class StatusCodePageTestCase(TestCase):
+    def setUp(self):
+        self.cli = Client()
+
+    def test_index_page(self):
+        rep = self.cli.get('/')
+        self.assertEqual(rep.status_code, 200)
+
+    def test_mini_dashboard_page(self):
+        rep = self.cli.get('/minidashboard')
+        self.assertEqual(rep.status_code, 200)
+
+    def test_sign_up_page(self):
+        rep = self.cli.get('/signup')
+        self.assertEqual(rep.status_code, 200)
+
+    def test_sign_out_page(self):
+        rep = self.cli.get('/signout')
+        self.assertEqual(rep.status_code, 200)
+
+class FormTestCase(TestCase):
+    def test_form_login(self):
+        data = {'pseudo': 'testUser', 'password': 'longpasswordtest'}
+        form = LoginForm(data=data)
+        self.assertTrue(form.is_valid())
+    
+    def test_form_sign_up(self):
+        data = {
+            'pseudo': 'testUser',
+            'first_name': 'USERTEST',
+            'last_name': 'Tester',
+            'email': 'test@fournisseur.com',
+            'password': 'longpasswordtest'
+        }
+        form = SignupForm(data=data)
+        self.assertTrue(form.is_valid())
