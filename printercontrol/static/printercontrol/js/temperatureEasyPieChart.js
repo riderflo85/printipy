dc24daeed5384d29a7f5cddd92a20c91f964f38d
Florent <grenaille.florent85@gmail.com>
2019-06-10 20:11:40 +0200

    Ajout des graphiques des température sur le dashboard

---
+++ printercontrol/static/printercontrol/js/temperatureEasyPieChart.js
@@ -0,0 +1,37 @@
+$(function() {
+    $('.chartNozzle').easyPieChart({
+        barColor: '#00cc00',
+        scaleColor: false,
+        scaleLength: 1,
+        lineWidth: 10,
+        size: 200,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent)+'°C');
+        }
+    });
+
+    // Update value temperature
+    /* var newTemp = 80;
+    // setTimeout(function() {
+    //     $('.chartNozzle').data('esayPieChart').update(newTemp);
+    }); */
+});
+
+$(function() {
+   $('.chartBed').easyPieChart({
+        barColor: '#ff6600',
+        scaleColor: false,
+        scaleLength: 10,
+        lineWidth: 10,
+        size: 200,
+        onStep: function (from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent) + '°C');
+        }
+    });
+
+    // Update value temperature
+    /* var newTemp = 80;
+    // setTimeout(function() {
+    //     $('.chartBed').data('esayPieChart').update(newTemp);
+    }); */
+});
