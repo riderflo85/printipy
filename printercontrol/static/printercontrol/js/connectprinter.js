63e57e1a8cccbe79d9e02e61dca6f367c4a7e82c
Florent <grenaille.florent85@gmail.com>
2019-06-17 12:04:33 +0200

    ajout du block 'try/catch' dans le JS pour éviter des erreurs dans la console

--- printercontrol/static/printercontrol/js/connectprinter.js
+++ printercontrol/static/printercontrol/js/connectprinter.js
@@ -1,27 +1,31 @@
-var btnConnect = document.getElementById('btnConnect');
-var statusPrinter = document.getElementById('printerStatus');
-var btnSaveConfig = document.getElementById('saveConfig');
+try {
+    var btnConnect = document.getElementById('btnConnect');
+    var statusPrinter = document.getElementById('printerStatus');
+    var btnSaveConfig = document.getElementById('saveConfig');
 
-btnSaveConfig.addEventListener('click', function (event) {
-    statusPrinter.textContent = 'Connecter';
-    statusPrinter.classList.replace('badge-noconnect', 'badge-connect');
+    btnSaveConfig.addEventListener('click', function (event) {
+        statusPrinter.textContent = 'Connecter';
+        statusPrinter.classList.replace('badge-noconnect', 'badge-connect');
 
-    btnConnect.textContent = 'Dé-connexion';
-    btnConnect.classList.replace('btn-primary', 'btn-info');
-    btnConnect.removeAttribute('data-toggle');
-    btnConnect.removeAttribute('data-target');
-});
+        btnConnect.textContent = 'Dé-connexion';
+        btnConnect.classList.replace('btn-primary', 'btn-info');
+        btnConnect.removeAttribute('data-toggle');
+        btnConnect.removeAttribute('data-target');
+    });
 
-btnConnect.addEventListener('click', function (event) {
-    if (this.textContent === 'Dé-connexion') {
-        statusPrinter.textContent = 'Non-connecter';
-        statusPrinter.classList.replace('badge-connect', 'badge-noconnect');
+    btnConnect.addEventListener('click', function (event) {
+        if (this.textContent === 'Dé-connexion') {
+            statusPrinter.textContent = 'Non-connecter';
+            statusPrinter.classList.replace('badge-connect', 'badge-noconnect');
 
-        this.textContent = 'Connexion';
-        this.classList.replace('btn-info', 'btn-primary');
-        this.setAttribute('data-toggle', 'modal');
-        this.setAttribute('data-target', '#modalConnect');
-    }else{
-        // Pass
-    }
-});
+            this.textContent = 'Connexion';
+            this.classList.replace('btn-info', 'btn-primary');
+            this.setAttribute('data-toggle', 'modal');
+            this.setAttribute('data-target', '#modalConnect');
+        } else {
+            // Pass
+        }
+    });
+} catch (error) {
+    // pass
+}
--- printercontrol/static/printercontrol/js/temperatureChartJs.js
+++ printercontrol/static/printercontrol/js/temperatureChartJs.js
@@ -1,37 +1,42 @@
-// Graph for nozzle
-var ctx = document.getElementById('chartNozzle').getContext('2d');
-var chartNozzle = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ['10h', '10h30', '11h', '11h30', '12h', '12h30', '13h'],
-        datasets: [{
-            label: 'Température de la buse',
-            backgroundColor: 'rgba(0, 204, 0, 0.5)',
-            borderColor: 'rgb(0, 204, 0)',
-            data: [0, 204, 200, 203, 204, 206, 204]
-        }]
-    },
+try {
+    // Graph for nozzle
+    var ctx = document.getElementById('chartNozzle').getContext('2d');
+    var chartNozzle = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
 
-    // Configuration options go here
-    options: {}
-});
+        // The data for our dataset
+        data: {
+            labels: ['10h', '10h30', '11h', '11h30', '12h', '12h30', '13h'],
+            datasets: [{
+                label: 'Température de la buse',
+                backgroundColor: 'rgba(0, 204, 0, 0.5)',
+                borderColor: 'rgb(0, 204, 0)',
+                data: [0, 204, 200, 203, 204, 206, 204]
+            }]
+        },
 
-// Graph for bed
-var ctx2 = document.getElementById('chartBed').getContext('2d');
-var chartBed = new Chart(ctx2, {
-    type: 'line',
-    data: {
-        labels: ['10h', '10h30', '11h', '11h30', '12h', '12h30', '13h'],
-        datasets: [{
-            label: 'Température du plateau',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 60, 60, 61, 57, 59, 60]
-        }]
-    },
+        // Configuration options go here
+        options: {}
+    });
 
-    options: {}
-});
+    // Graph for bed
+    var ctx2 = document.getElementById('chartBed').getContext('2d');
+    var chartBed = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: ['10h', '10h30', '11h', '11h30', '12h', '12h30', '13h'],
+            datasets: [{
+                label: 'Température du plateau',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 60, 60, 61, 57, 59, 60]
+            }]
+        },
+
+        options: {}
+    });
+} catch (error) {
+    //pass
+}
