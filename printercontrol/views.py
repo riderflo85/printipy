9ef701bd619cb3a9eaff354ab5aaf914a1807a25
Florent <grenaille.florent85@gmail.com>
2020-03-28 15:58:30 +0100

    Added new view for moving the printer axe

--- printercontrol/views.py
+++ printercontrol/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
 def dashboard(request):
     return render(request, 'printercontrol/dashboard.html')
 
@@ -9,3 +10,16 @@ def controlpack(request):
 
 def prepare(request):
     return render(request, 'printercontrol/prepare.html')
+
+def mouv(request):
+    if request.method == 'POST':
+        speed = request.POST['speed']
+        distance = request.POST['distance']
+        type_pos = request.POST['type_pos']
+        mouv_type = request.POST['mouv_type']
+        axe = request.POST['axe']
+
+        if mouv_type == 'positive':
+            distance = f"{distance}"
+        elif mouv_type == 'negative':
+            distance = f"-{distance}"
