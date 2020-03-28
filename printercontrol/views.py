4f5024f5d6b4cfa2988bb935a4c647a9867a8d3a
Florent <grenaille.florent85@gmail.com>
2020-03-28 18:04:30 +0100

    Added a login_required decorator in the views.py (printercontrol)

--- printercontrol/views.py
+++ printercontrol/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from .models import Printer
 
 
 @login_required
