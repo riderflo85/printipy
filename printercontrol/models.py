e004e53db1829017b91c8862e5f59d02ce2ee234
Florent <grenaille.florent85@gmail.com>
2020-03-29 16:40:37 +0200

    Update model 'Printer' for add the 'status' field

--- printercontrol/models.py
+++ printercontrol/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class Printer(models.Model):
     name = models.CharField(max_length=150)
     model = models.CharField(max_length=150)
+    status = models.CharField(max_length=12, default='disconnected')
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
