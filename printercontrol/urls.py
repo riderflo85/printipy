dd346849cf963e5e4b319b1c47c4cce747bb1974
Florent <grenaille.florent85@gmail.com>
2020-03-29 16:01:24 +0200

    Added the urls

--- printercontrol/urls.py
+++ printercontrol/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('controlpack', views.controlpack, name='controlpack'),
     path('prepare', views.prepare, name='prepare'),
+    path('connect_printer', views.connect_printer, name='connect_printer'),
+    path('disconnect_printer', views.disconnect, name='disconnect_printer'),
+    path('mouv', views.mouv, name='mouv'),
 ]
